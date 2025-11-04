9102d8d5efc2b429a3555b942869804b6472072e
szzdmj <169899174+szzdmj@users.noreply.github.com>
2025-11-04 11:15:40 +0800

    Add package and import statements to WebShellActivity

--- app/src/main/java/com/brouken/player/web/WebShellActivity.java
+++ app/src/main/java/com/brouken/player/web/WebShellActivity.java
@@ -1,1 +1,17 @@
+package com.brouken.player.web;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompat
